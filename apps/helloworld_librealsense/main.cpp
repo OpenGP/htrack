bdb55da794ec73a8514f417197bb8601eeb3b23a
r4ghu <raghu_sri95@yahoo.in>
2016-08-25 14:03:40 -0700

    modified CMakeLists and tested on PointCloud functionality of librealsense

--- apps/helloworld_librealsense/main.cpp
+++ apps/helloworld_librealsense/main.cpp
@@ -1,16 +1,41 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2015 Intel Corporation. All Rights Reserved.
 
-/////////////////////////////////////////////////////
-// librealsense tutorial #1 - Accessing depth data //
-/////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+// librealsense tutorial #3 - Point cloud generation //
+///////////////////////////////////////////////////////
 
 // First include the librealsense C++ header file
 #include <librealsense/rs.hpp>
 #include <cstdio>
 
+// Also include GLFW to allow for graphical display
+#define GLFW_INCLUDE_GLU
+#include <GLFW/glfw3.h>
+
+double yaw, pitch, lastX, lastY; int ml;
+static void on_mouse_button(GLFWwindow * win, int button, int action, int mods)
+{
+    if(button == GLFW_MOUSE_BUTTON_LEFT) ml = action == GLFW_PRESS;
+}
+static double clamp(double val, double lo, double hi) { return val < lo ? lo : val > hi ? hi : val; }
+static void on_cursor_pos(GLFWwindow * win, double x, double y)
+{
+    if(ml)
+    {
+        yaw = clamp(yaw - (x - lastX), -120, 120);
+        pitch = clamp(pitch + (y - lastY), -80, 80);
+    }
+    lastX = x;
+    lastY = y;
+}
+
 int main() try
 {
+    // Turn on logging. We can separately enable logging to console or to file, and use different severity filters for each.
+    rs::log_to_console(rs::log_severity::warn);
+    //rs::log_to_file(rs::log_severity::debug, "librealsense.log");
+
     // Create a context object. This object owns the handles to all connected realsense devices.
     rs::context ctx;
     printf("There are %d connected RealSense devices.\n", ctx.get_device_count());
@@ -22,48 +47,88 @@ int main() try
     printf("    Serial number: %s\n", dev->get_serial());
     printf("    Firmware version: %s\n", dev->get_firmware_version());
 
-    // Configure depth to run at VGA resolution at 30 frames per second
-    dev->enable_stream(rs::stream::depth, 640, 480, rs::format::z16, 30);
+    // Configure depth and color to run with the device's preferred settings
+    dev->enable_stream(rs::stream::depth, rs::preset::best_quality);
+    dev->enable_stream(rs::stream::color, rs::preset::best_quality);
     dev->start();
 
-    // Determine depth value corresponding to one meter
-    const uint16_t one_meter = static_cast<uint16_t>(1.0f / dev->get_depth_scale());
-
-    while(true)
+    // Open a GLFW window to display our output
+    glfwInit();
+    GLFWwindow * win = glfwCreateWindow(1280, 960, "librealsense tutorial #3", nullptr, nullptr);
+    glfwSetCursorPosCallback(win, on_cursor_pos);
+    glfwSetMouseButtonCallback(win, on_mouse_button);
+    glfwMakeContextCurrent(win);
+    while(!glfwWindowShouldClose(win))
     {
-        // This call waits until a new coherent set of frames is available on a device
-        // Calls to get_frame_data(...) and get_frame_timestamp(...) on a device will return stable values until wait_for_frames(...) is called
+        // Wait for new frame data
+        glfwPollEvents();
         dev->wait_for_frames();
 
-        // Retrieve depth data, which was previously configured as a 640 x 480 image of 16-bit depth values
-        const uint16_t * depth_frame = reinterpret_cast<const uint16_t *>(dev->get_frame_data(rs::stream::depth));
+        // Retrieve our images
+        const uint16_t * depth_image = (const uint16_t *)dev->get_frame_data(rs::stream::depth);
+        const uint8_t * color_image = (const uint8_t *)dev->get_frame_data(rs::stream::color);
 
-        // Print a simple text-based representation of the image, by breaking it into 10x20 pixel regions and and approximating the coverage of pixels within one meter
-        char buffer[(640/10+1)*(480/20)+1];
-        char * out = buffer;
-        int coverage[64] = {};
-        for(int y=0; y<480; ++y)
+        // Retrieve camera parameters for mapping between depth and color
+        rs::intrinsics depth_intrin = dev->get_stream_intrinsics(rs::stream::depth);
+        rs::extrinsics depth_to_color = dev->get_extrinsics(rs::stream::depth, rs::stream::color);
+        rs::intrinsics color_intrin = dev->get_stream_intrinsics(rs::stream::color);
+        float scale = dev->get_depth_scale();
+
+        // Set up a perspective transform in a space that we can rotate by clicking and dragging the mouse
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        gluPerspective(60, (float)1280/960, 0.01f, 20.0f);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(0,0,0, 0,0,1, 0,-1,0);
+        glTranslatef(0,0,+0.5f);
+        glRotated(pitch, 1, 0, 0);
+        glRotated(yaw, 0, 1, 0);
+        glTranslatef(0,0,-0.5f);
+
+        // We will render our depth data as a set of points in 3D space
+        glPointSize(2);
+        glEnable(GL_DEPTH_TEST);
+        glBegin(GL_POINTS);
+
+        for(int dy=0; dy<depth_intrin.height; ++dy)
         {
-            for(int x=0; x<640; ++x)
+            for(int dx=0; dx<depth_intrin.width; ++dx)
             {
-                int depth = *depth_frame++;
-                if(depth > 0 && depth < one_meter) ++coverage[x/10];
-            }
+                // Retrieve the 16-bit depth value and map it into a depth in meters
+                uint16_t depth_value = depth_image[dy * depth_intrin.width + dx];
+                float depth_in_meters = depth_value * scale;
 
-            if(y%20 == 19)
-            {
-                for(int & c : coverage)
+                // Skip over pixels with a depth value of zero, which is used to indicate no data
+                if(depth_value == 0) continue;
+
+                // Map from pixel coordinates in the depth image to pixel coordinates in the color image
+                rs::float2 depth_pixel = {(float)dx, (float)dy};
+                rs::float3 depth_point = depth_intrin.deproject(depth_pixel, depth_in_meters);
+                rs::float3 color_point = depth_to_color.transform(depth_point);
+                rs::float2 color_pixel = color_intrin.project(color_point);
+
+                // Use the color from the nearest color pixel, or pure white if this point falls outside the color image
+                const int cx = (int)std::round(color_pixel.x), cy = (int)std::round(color_pixel.y);
+                if(cx < 0 || cy < 0 || cx >= color_intrin.width || cy >= color_intrin.height)
                 {
-                    *out++ = " .:nhBXWW"[c/25];
-                    c = 0;
+                    glColor3ub(255, 255, 255);
                 }
-                *out++ = '\n';
+                else
+                {
+                    glColor3ubv(color_image + (cy * color_intrin.width + cx) * 3);
+                }
+
+                // Emit a vertex at the 3D location of this depth pixel
+                glVertex3f(depth_point.x, depth_point.y, depth_point.z);
             }
         }
-        *out++ = 0;
-        printf("\n%s", buffer);
+        glEnd();
+
+        glfwSwapBuffers(win);
     }
-    
+
     return EXIT_SUCCESS;
 }
 catch(const rs::error & e)
